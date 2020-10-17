c8bf67e6a587049b5d94998b3c7bf8ebf346585e
Eric Dam <dam.eric3@gmail.com>
2020-10-17 15:08:18 -0700

    added the News Details page, need to remove the hardcoded text and image size once the back end is working

---
+++ app/src/main/java/com/example/verma_dam/NewsDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.verma_dam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_details);
+    }
+}
