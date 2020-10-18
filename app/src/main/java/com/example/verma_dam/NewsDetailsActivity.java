db8ce9a7c08e5eccff49b3f8b3c1c364209f0667
yogiduzit <yverma2@my.bcit.ca>
2020-10-18 16:37:57 -0700

    Hooked up list and details page and improved ui

--- app/src/main/java/com/example/verma_dam/NewsDetailsActivity.java
+++ app/src/main/java/com/example/verma_dam/NewsDetailsActivity.java
@@ -2,13 +2,93 @@ package com.example.verma_dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.format.DateUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.verma_dam.data.NewsItem;
+import com.example.verma_dam.data.Source;
+
+import java.net.URL;
+import java.sql.Timestamp;
 
 public class NewsDetailsActivity extends AppCompatActivity {
+
+    private final static String ORIGINAL_STORY_CAPTION = "Click here for original story";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
+        setupNewsDetails();
+    }
+
+    private void setupNewsDetails() {
+        NewsItem newsItem = getIntent().getParcelableExtra(NewsListActivity.PARCEL_NAME);
+
+        setupTitle(newsItem.getTitle());
+        setupDescription(newsItem.getDescription());
+        setupArticlePicture(newsItem.getUrltoImage());
+        setupSource(newsItem.getSource());
+        setupAuthor(newsItem.getAuthor());
+        setupPublishedAt(newsItem.getPublishedAt());
+        setupContent(newsItem.getContent());
+        setupOriginalURL(newsItem.getUrl());
+    }
+
+    private void setupTitle(String titleInput) {
+        TextView title = findViewById(R.id.title);
+        title.setText(titleInput);
+    }
+
+    private void setupDescription(String descInput) {
+        TextView description = findViewById(R.id.description);
+        description.setText(descInput);
+    }
+
+    private void setupArticlePicture(String urlToImage) {
+        ImageView thumbnail = findViewById(R.id.article_photo);
+        if (urlToImage != null) {
+            Glide.with(getApplicationContext())
+                    .load(urlToImage)
+                    .into(thumbnail);
+        }
+    }
+
+    private void setupSource(Source source) {
+        TextView newsSource = findViewById(R.id.source);
+        newsSource.setText(source.getName());
+    }
+
+    private void setupAuthor(String author) {
+        TextView newsAuthor = findViewById(R.id.author);
+        newsAuthor.setText(author);
+    }
+
+    private void setupPublishedAt(Timestamp publishedAt) {
+        TextView publishedDate = findViewById(R.id.publish_date);
+        publishedDate.setText(DateUtils.getRelativeTimeSpanString(publishedAt.getTime()));
+    }
+
+    private void setupContent(String content) {
+        TextView newsContent = findViewById(R.id.content);
+        newsContent.setText(Html.fromHtml(content, Build.VERSION_CODES.N));
+    }
+
+    private void setupOriginalURL(URL url) {
+        if (url != null) {
+            String link = url.toString();
+            TextView originalStory = findViewById(R.id.original_story);
+            String originalURL = "<a href=" + link + ">" + ORIGINAL_STORY_CAPTION + "</a>";
+            originalStory.setText(Html.fromHtml(originalURL, Build.VERSION_CODES.N));
+            originalStory.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
     }
 }
--- app/src/main/java/com/example/verma_dam/NewsListActivity.java
+++ app/src/main/java/com/example/verma_dam/NewsListActivity.java
@@ -2,9 +2,12 @@ package com.example.verma_dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,11 +18,11 @@ import com.example.verma_dam.http.HttpHandler;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 public class NewsListActivity extends AppCompatActivity {
 
     private static final String API_KEY = "b4350f204db044389a45332ae2003a66";
+    static final String PARCEL_NAME = "newsItem";
     // URL to get news
     private static String SERVICE_URL = "https://newsapi.org/v2/everything?q=bitcoin&from=2020-10-14&sortBy=publishedAt&apiKey=" + API_KEY;
 
@@ -52,7 +55,6 @@ public class NewsListActivity extends AppCompatActivity {
                 // this step is needed to wrap the JSON array inside
                 // a JSON object that looks like this { "toons": . . . . }
                 Gson gson = new Gson();
-                Map response = gson.fromJson(jsonStr, Map.class);
 
                 BaseNewsItem baseNewsItem = gson.fromJson(jsonStr, BaseNewsItem.class);
                 newsItemList = baseNewsItem.getNewsItems();
@@ -90,6 +92,19 @@ public class NewsListActivity extends AppCompatActivity {
 
         newsItemList = new ArrayList<NewsItem>();
         lv = findViewById(R.id.news_item_list);
+        setItemClickListener();
         new GetNewsItems().execute();
+    }
+
+    private void setItemClickListener() {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                NewsItem newsItem = newsItemList.get(position);
+                Intent newsDetails = new Intent(getApplicationContext(), NewsDetailsActivity.class);
+                newsDetails.putExtra(PARCEL_NAME, newsItem);
+                startActivity(newsDetails);
+            }
+        });
     }
 }
